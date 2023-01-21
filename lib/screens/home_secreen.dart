e73c86332bbd446a0ceaf522d45be48eba350d92
pachperdev <pachperdev@gmail.com>
2023-01-21 12:41:26 -0500

    implementar Row Actions

--- lib/screens/home_secreen.dart
+++ lib/screens/home_secreen.dart
@@ -9,6 +9,8 @@ class HomeScreen extends StatelessWidget {
       body: Column(
         children: const [
           Banner(),
+          Title(),
+          RowActions(),
         ],
       ),
     );
@@ -28,6 +30,112 @@ class Banner extends StatelessWidget {
       child: Image(
         fit: BoxFit.cover,
         image: AssetImage('lib/assets/flutter_app_design_main.jpg'),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  const Title({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                'Senior Flutter Developer',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Cali, Colombia',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: const [
+              Icon(
+                Icons.star,
+                color: Colors.red,
+              ),
+              Text(
+                '41',
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class RowActions extends StatelessWidget {
+  const RowActions({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Column(
+            children: const [
+              Icon(
+                Icons.call,
+                color: Colors.blue,
+                size: 30,
+              ),
+              Text(
+                'Call',
+                style: TextStyle(color: Colors.blue, fontSize: 16),
+              )
+            ],
+          ),
+          Column(
+            children: const [
+              Icon(
+                Icons.call,
+                color: Colors.blue,
+                size: 30,
+              ),
+              Text(
+                'Call',
+                style: TextStyle(color: Colors.blue, fontSize: 16),
+              )
+            ],
+          ),
+          Column(
+            children: const [
+              Icon(
+                Icons.call,
+                color: Colors.blue,
+                size: 30,
+              ),
+              Text(
+                'Call',
+                style: TextStyle(color: Colors.blue, fontSize: 16),
+              )
+            ],
+          ),
+        ],
       ),
     );
   }
